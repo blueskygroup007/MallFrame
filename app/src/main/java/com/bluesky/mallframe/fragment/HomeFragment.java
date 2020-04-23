b1295d2c9c0387c2d16487764b624bf2e70222bc
bluesky <16733682@qq.com>
2020-04-23 17:21:46 +0800

    选定datepicker控件,正在测试

--- app/src/main/java/com/bluesky/mallframe/fragment/HomeFragment.java
+++ app/src/main/java/com/bluesky/mallframe/fragment/HomeFragment.java
@@ -1,12 +1,24 @@
 package com.bluesky.mallframe.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 
 import com.blankj.utilcode.util.LogUtils;
 import com.bluesky.mallframe.R;
 import com.bluesky.mallframe.base.BaseFragment;
+import com.zyyoona7.picker.DatePickerView;
+import com.zyyoona7.picker.base.BaseDatePickerView;
+import com.zyyoona7.picker.listener.OnDateSelectedListener;
+import com.zyyoona7.wheel.WheelView;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author BlueSky
@@ -15,10 +27,20 @@ import com.bluesky.mallframe.base.BaseFragment;
  */
 public class HomeFragment extends BaseFragment {
     private TextView mTvTitle;
+    private WheelView<String> mWvWeek;
+    private DatePickerView mDpvDate;
+
+    private List<String> mListWeek = Arrays.asList("星期一", "星期一", "星期一", "星期一", "星期一", "星期一", "星期一");
+
 
     @Override
     protected void initEvent() {
-
+        mDpvDate.setOnDateSelectedListener(new OnDateSelectedListener(){
+            @Override
+            public void onDateSelected(BaseDatePickerView datePickerView, int year, int month, int day, @Nullable Date date) {
+//                Snackbar.make();
+            }
+        });
     }
 
     @Override
@@ -26,11 +48,14 @@ public class HomeFragment extends BaseFragment {
         mTvTitle.setText("这是主页面fragment!");
         LogUtils.d("主页面的fragment的数据被初始化了");
 
+        mWvWeek.setData(mListWeek);
     }
 
     @Override
     protected void initView(View view, Bundle savedInstanceState) {
         mTvTitle = view.findViewById(R.id.tv_fragment_home_title);
+        mWvWeek = view.findViewById(R.id.wv_week);
+        mDpvDate = view.findViewById(R.id.dpv_date);
     }
 
     @Override
